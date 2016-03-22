dbb749be688e8dc397a3262cbadb11b5a7dcb7a0
Tyler Adams <tyleraadams@gmail.com>
2016-03-22 09:01:55 -0400

    added documentation to alertsManager

--- app/managers/alerts.js
+++ app/managers/alerts.js
@@ -8,6 +8,10 @@ const AlertManager = function () {
   this.alert = null;
 };
 
+/*
+  AlertManager init finds the last two minutes worth of uptime data from DB, calculates the average,
+  and then runs its own determineAlertState fn on that avg every ten seconds
+*/
 AlertManager.prototype.init = function () {
   const self = this;
   setInterval(function () {
@@ -23,6 +27,11 @@ AlertManager.prototype.init = function () {
   }, 10000);
 };
 
+/*
+  AlertManager createAlert creates an instance Alert model with load average,
+  saves to DB, and sets alert state to true
+  @parameter {Number} dataPont the avg load
+ */
 AlertManager.prototype.createAlert =  function (dataPoint) {
   let alert = new Alert({
     load: dataPoint
@@ -32,6 +41,10 @@ AlertManager.prototype.createAlert =  function (dataPoint) {
   alert.save();
 };
 
+/*
+  AlertManager recoverAlert finds the latest alert and calls updateAlertWithRecovery on it
+  and then turns off alert state
+ */
 AlertManager.prototype.recoverAlert = function () {
   const self = this;
   if (this.alert) {
@@ -49,12 +62,22 @@ AlertManager.prototype.recoverAlert = function () {
   this.isInAlertState = false;
 };
 
+/*
+  AlertManager updateAlertWithRecovery assigns recovered_at datetime to now and saves to DB
+  @parameter {Object} alertObject an instance of Alert model that has already been instantiated
+*/
 AlertManager.prototype.updateAlertWithRecovery = function (alertObject) {
   let now = new Date();
   alertObject.recovered_at = now;
   alertObject.save();
 };
 
+/*
+  AlertManager determineAlertState creates an alert if average is higher than alert threshold and we
+  we are not in alert state, recovers an alert if we are in alert state and average has dipped below
+  threshold, or does nothing
+  @parameter {Number} avg  average load over the last two minutes
+*/
 AlertManager.prototype.determineAlertState =  function (avg) {
   if (avg > alertThreshold && !this.isInAlertState) {
     this.createAlert(avg);
